95776986968d99bc87631e2e8a0c3ab01b2954d9
chencch <chencch@amazon.com>
2025-01-10 15:04:40 +0800

    Initial commit

---
+++ test/say-hi.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SayHi from '../lib/say-hi-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/say-hi-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SayHi.SayHiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
